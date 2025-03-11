d497531ea7ccba3758fa0d8378687ec474c1eb67
Maryame <iguizoulmaryame@gmail.com>
2025-03-11 11:29:42 +0100

    creation des interfaces

--- GestionProject/src/main/java/com/example/GestionProject/service/EpicService.java
+++ GestionProject/src/main/java/com/example/GestionProject/service/EpicService.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class EpicService {
+public class EpicService implements EpicInterface {
 
     private final EpicRepository epicRepository;
     private final ProductBacklogRepository productBacklogRepository;
